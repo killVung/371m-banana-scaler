054648d84e7f1ef230917aacf1d96a7a7ddef874
killvung <kiulam125@gmail.com>
2016-03-22 18:40:14 -0500

    Add activities for both buttons

--- app/src/main/java/com/example/niclam/banana_scaler/MainMenu.java
+++ app/src/main/java/com/example/niclam/banana_scaler/MainMenu.java
@@ -1,15 +1,18 @@
 package com.example.niclam.banana_scaler;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainMenu extends AppCompatActivity {
+    private final String TAG = "MainMenu";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,5 +40,15 @@ public class MainMenu extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void startTakingPhotos(View fuck){
+        startActivityForResult(new Intent(this, TakingPhotosActivity.class), 0);
+        Log.d(TAG,"Transferring to taking photos function");
+    }
+
+    public void startViewingPhotos(View you){
+        startActivityForResult(new Intent(this, ViewingPhotosActivity.class), 0);
+        Log.d(TAG,"Transferring to testing viewing photos function");
     }
 }
